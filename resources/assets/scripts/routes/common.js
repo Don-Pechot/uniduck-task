cacc60e0e885042a92472eb6c3612f6ca1f07a48
Goran Pećirko <goran@q-software.com>
2018-03-25 20:24:27 +0200

    header

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,16 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    (function menuToggle() {
+      var toggle = $('.jsToggleMenu');
+      var nav = $('.header-main-menu__nav');
+
+      toggle.on('click', function() {
+        nav.toggleClass('isVisibleMobile');
+      })
+    })();
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,24 @@
 <header class="banner">
-  <div class="sage-wrapper">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+  <div class="sage-wrapper container">
+    <div class="row">
+      <div class="header-logo">
+        @if (function_exists('the_custom_logo'))
+          {{ the_custom_logo() }}
+        @else
+          <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+        @endif
+      </div>
+
+      <div class="header-main-menu">
+        <nav class="header-main-menu__nav nav-primary">
+          @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+          @endif
+        </nav>
+        <div class="header-main-menu__toggle jsToggleMenu">
+          <span class="udck-menu"></span>
+        </div>
+      </div>
+    </div>
   </div>
 </header>
