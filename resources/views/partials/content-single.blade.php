4bad9cbee2d47e61a626343ffb67d005917f655b
Goran Pećirko <goran@q-software.com>
2018-03-26 23:51:55 +0200

    single post typography and styles WIP

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -16,11 +16,33 @@
       </div>
     </div>
   </header>
+
+
   <div class="entry-content">
-    @php(the_content())
+
+    @if (get_field('post_featured_text', $post->ID))
+      <div class="container">
+        <div class="row">
+          <section class="entry-content__featured">
+            {!! get_field('post_featured_text', $post->ID) !!}
+          </section>
+        </div>
+      </div>
+    @endif
+
+    <div class="container">
+      <div class="row">
+        <div class="entry-content__main">
+          <div class="row-align-around">
+            @php(the_content())
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php(comments_template('/partials/comments.blade.php'))
+  {{-- @php(comments_template('/partials/comments.blade.php')) --}}
 </article>
